ff5fc42a99ae9207e91c29d506280141c3fba64e
Masahiro Yamamoto <cmvng00@gmail.com>
2025-05-26 03:49:36 +0900

    fix:デフォルト制約消した

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -4,9 +4,9 @@ CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     email VARCHAR(255) NOT NULL UNIQUE,
     password TEXT NOT NULL,
-    timezone VARCHAR(64) NOT NULL DEFAULT 'Asia/Tokyo',
-    theme_color theme_color_enum NOT NULL DEFAULT 'dark',
-    language VARCHAR(5) NOT NULL DEFAULT 'ja',
+    timezone VARCHAR(64) NOT NULL,
+    theme_color theme_color_enum NOT NULL,
+    language VARCHAR(5) NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 )
