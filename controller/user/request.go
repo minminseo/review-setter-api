22eafef3d8575ed36a105775d8d97548b1e0f7bc
Masahiro Yamamoto <cmvng00@gmail.com>
2025-05-27 05:38:57 +0900

    feat: コントローラー層実装

---
+++ controller/user/request.go
@@ -0,0 +1,27 @@
+package user
+
+type sighInUserRequest struct {
+	Email      string `json:"email"`
+	Password   string `json:"password"`
+	Timezone   string `json:"timezone"`
+	ThemeColor string `json:"theme_color"`
+	Language   string `json:"language"`
+}
+
+type logInUserRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type updateUserRequest struct {
+	ID         string `json:"id"`
+	Email      string `json:"email"`
+	Timezone   string `json:"timezone"`
+	ThemeColor string `json:"theme_color"`
+	Language   string `json:"language"`
+}
+
+type updatePasswordRequest struct {
+	ID       string `json:"id"`
+	Password string `json:"password"`
+}
---
+++ controller/user/user_interface.go
@@ -0,0 +1,13 @@
+package user
+
+import "github.com/labstack/echo/v4"
+
+type IUserController interface {
+	SignUp(c echo.Context) error
+	LogIn(c echo.Context) error
+	LogOut(c echo.Context) error
+	CsrfToken(c echo.Context) error
+	GetUserSetting(c echo.Context) error
+	UpdateSetting(c echo.Context) error
+	UpdatePassword(c echo.Context) error
+}
