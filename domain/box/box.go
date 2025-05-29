e5a3ea16a40270d6902e4820c4d513e296a507b6
Masahiro Yamamoto <cmvng00@gmail.com>
2025-05-30 04:35:17 +0900

    fix: 作成日と更新日追加・パターンID比較ロジック追加・ReconstructBox定義

--- domain/box/box.go
+++ domain/box/box.go
@@ -1,13 +1,19 @@
 package box
 
-import validation "github.com/go-ozzo/ozzo-validation/v4"
+import (
+	"time"
 
-type box struct {
-	id         string
-	userID     string
-	categoryID string
-	patternID  string
-	name       string
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+)
+
+type Box struct {
+	ID           string
+	UserID       string
+	CategoryID   string
+	PatternID    string
+	Name         string
+	RegisteredAt time.Time
+	EditedAt     time.Time
 }
 
 func NewBox(
@@ -16,25 +22,70 @@ func NewBox(
 	categoryID string,
 	patternID string,
 	name string,
-) (*box, error) {
-	b := &box{
-		id:         id,
-		userID:     userID,
-		categoryID: categoryID,
-		patternID:  patternID,
-		name:       name,
-	}
-	if err := b.Validate(); err != nil {
+	registeredAt time.Time,
+	editedAt time.Time,
+) (*Box, error) {
+
+	if err := validateName(name); err != nil {
 		return nil, err
+	}
+
+	b := &Box{
+		ID:           id,
+		UserID:       userID,
+		CategoryID:   categoryID,
+		PatternID:    patternID,
+		Name:         name,
+		RegisteredAt: registeredAt,
+		EditedAt:     editedAt,
+	}
+
+	return b, nil
+}
+
+func ReconstructBox(
+	id string,
+	userID string,
+	categoryID string,
+	patternID string,
+	name string,
+	registeredAt time.Time,
+	editedAt time.Time,
+) (*Box, error) {
+	b := &Box{
+		ID:           id,
+		UserID:       userID,
+		CategoryID:   categoryID,
+		PatternID:    patternID,
+		Name:         name,
+		RegisteredAt: registeredAt,
+		EditedAt:     editedAt,
 	}
 	return b, nil
 }
 
-func (b *box) Validate() error {
-	return validation.ValidateStruct(b,
-		validation.Field(
-			&b.name,
-			validation.Required.Error("ボックス名は必須です"),
-		),
+func validateName(name string) error {
+	return validation.Validate(
+		name,
+		validation.Required.Error("カテゴリー名は必須です"),
 	)
+}
+
+func (b *Box) Set(patternID string, name string, editedAt time.Time) (bool, error) {
+	var isSamePattern bool
+	if b.PatternID == patternID {
+		isSamePattern = true
+	} else {
+		isSamePattern = false
+	}
+
+	if err := validateName(name); err != nil {
+		return isSamePattern, err
+	}
+
+	b.PatternID = patternID
+	b.Name = name
+	b.EditedAt = editedAt
+
+	return isSamePattern, nil
 }
