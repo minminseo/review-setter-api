2a59ac23229115216ec91f68aab6f45f79d5f2a5
Masahiro Yamamoto <cmvng00@gmail.com>
2025-05-30 04:29:34 +0900

    fix: アプリで使うソート用の作成日と更新日という概念を追加

--- domain/category/category.go
+++ domain/category/category.go
@@ -1,17 +1,25 @@
 package category
 
-import validation "github.com/go-ozzo/ozzo-validation/v4"
+import (
+	"time"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+)
 
 type Category struct {
-	ID     string
-	UserID string
-	Name   string
+	ID           string
+	UserID       string
+	Name         string
+	RegisteredAt time.Time
+	EditedAt     time.Time
 }
 
 func NewCategory(
 	id string,
 	userID string,
 	name string,
+	registeredAt time.Time,
+	editedAt time.Time,
 ) (*Category, error) {
 
 	if err := validateName(name); err != nil {
@@ -19,9 +27,11 @@ func NewCategory(
 	}
 
 	c := &Category{
-		ID:     id,
-		UserID: userID,
-		Name:   name,
+		ID:           id,
+		UserID:       userID,
+		Name:         name,
+		RegisteredAt: registeredAt,
+		EditedAt:     editedAt,
 	}
 
 	return c, nil
@@ -31,11 +41,15 @@ func ReconstructCategory(
 	id string,
 	userID string,
 	name string,
+	registeredAt time.Time,
+	editedAt time.Time,
 ) (*Category, error) {
 	c := &Category{
-		ID:     id,
-		UserID: userID,
-		Name:   name,
+		ID:           id,
+		UserID:       userID,
+		Name:         name,
+		RegisteredAt: registeredAt,
+		EditedAt:     editedAt,
 	}
 	return c, nil
 }
@@ -47,11 +61,12 @@ func validateName(name string) error {
 	)
 }
 
-func (c *Category) Set(name string) error {
+func (c *Category) Set(name string, editedAt time.Time) error {
 	if err := validateName(name); err != nil {
 		return err
 	}
 
 	c.Name = name
+	c.EditedAt = editedAt
 	return nil
 }
