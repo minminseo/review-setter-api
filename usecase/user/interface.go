ba8ed6e3184b836a153858d61bf7aeac88ee1fc5
Masahiro Yamamoto <cmvng00@gmail.com>
2025-06-02 23:48:39 +0900

    fix: context追加

--- usecase/user/interface.go
+++ usecase/user/interface.go
@@ -1,9 +1,11 @@
 package user
 
+import "context"
+
 type IUserUsecase interface {
-	SignUp(user CreateUserInput) (*CreateUserOutput, error)
-	LogIn(user LoginUserInput) (*LoginUserOutput, error)
-	GetUserSetting(userID string) (*GetUserOutput, error)
-	UpdateSetting(user UpdateUserInput) (*UpdateUserOutput, error)
-	UpdatePassword(userID, password string) error
+	SignUp(ctx context.Context, user CreateUserInput) (*CreateUserOutput, error)
+	LogIn(ctx context.Context, user LoginUserInput) (*LoginUserOutput, error)
+	GetUserSetting(ctx context.Context, userID string) (*GetUserOutput, error)
+	UpdateSetting(ctx context.Context, user UpdateUserInput) (*UpdateUserOutput, error)
+	UpdatePassword(ctx context.Context, userID, password string) error
 }
