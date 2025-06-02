bbaec7cca6c9b16b16d6022e1731248260b53a69
Masahiro Yamamoto <cmvng00@gmail.com>
2025-06-03 03:42:16 +0900

    fix: トランザクション用の処理のインスタンスをパターン系にDI。パターン系のDI定義

--- cmd/main.go
+++ cmd/main.go
@@ -14,6 +14,9 @@ import (
 	boxController "github.com/minminseo/recall-setter/controller/box"
 	boxUsecase "github.com/minminseo/recall-setter/usecase/box"
 
+	patternController "github.com/minminseo/recall-setter/controller/pattern"
+	patternUsecase "github.com/minminseo/recall-setter/usecase/pattern"
+
 	"github.com/minminseo/recall-setter/infrastructure/db"
 	"github.com/minminseo/recall-setter/infrastructure/repository"
 	"github.com/minminseo/recall-setter/router"
@@ -23,24 +26,30 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	pool, _, err := db.NewDB(ctx)
+	pool, err := db.NewDB(ctx)
 	if err != nil {
 		log.Fatalf("DB接続に失敗しました: %v", err)
 	}
 	defer pool.Close()
 
-	userRepository := repository.NewUserRepository(pool)
+	transactionManager := repository.NewTransactionManager(pool)
+
+	userRepository := repository.NewUserRepository()
 	userUsecase := userUsecase.NewUserUsecase(userRepository)
 	userController := userController.NewUserController(userUsecase)
 
-	categoryRepository := repository.NewCategoryRepository(pool)
+	categoryRepository := repository.NewCategoryRepository()
 	categoryUsecase := categoryUsecase.NewCategoryUsecase(categoryRepository)
 	categoryController := categoryController.NewCategoryController(categoryUsecase)
 
-	boxRepository := repository.NewBoxRepository(pool)
+	boxRepository := repository.NewBoxRepository()
 	boxUsecase := boxUsecase.NewBoxUsecase(boxRepository)
 	boxController := boxController.NewBoxController(boxUsecase)
 
-	e := router.NewRouter(userController, categoryController, boxController)
+	patternRepository := repository.NewPatternRepository()
+	patternUsecase := patternUsecase.NewPatternUsecase(patternRepository, transactionManager)
+	patternController := patternController.NewPatternController(patternUsecase)
+
+	e := router.NewRouter(userController, categoryController, boxController, patternController)
 	e.Logger.Fatal(e.Start(":8080"))
 }
