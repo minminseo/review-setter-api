57f3f06baebfd249b54860d8c3ec95d21d8d042e
Masahiro Yamamoto <cmvng00@gmail.com>
2025-06-03 03:34:47 +0900

    fix: ctx追加（TX用）

--- domain/box/box_repository.go
+++ domain/box/box_repository.go
@@ -1,21 +1,23 @@
 package box
 
+import "context"
+
 type BoxCountGroupedByCategory struct {
 	CategoryID string
 	Count      int
 }
 
-type BoxRepository interface {
-	Create(box *Box) error
+type IBoxRepository interface {
+	Create(ctx context.Context, box *Box) error
 
 	// カテゴリー内画面
 	// 各ボックス編集画面で表示する情報もこれを使う
-	GetAllByCategoryID(categoryID string, userID string) ([]*Box, error)
-	GetByID(boxID string, categoryID string, userID string) (*Box, error)
+	GetAllByCategoryID(ctx context.Context, categoryID string, userID string) ([]*Box, error)
+	GetByID(ctx context.Context, boxID string, categoryID string, userID string) (*Box, error)
 
-	Update(box *Box) error
-	UpdateWithPatternID(box *Box) (int64, error)
-	Delete(boxID string, categoryID string, userID string) error
+	Update(ctx context.Context, box *Box) error
+	UpdateWithPatternID(ctx context.Context, box *Box) (int64, error)
+	Delete(ctx context.Context, boxID string, categoryID string, userID string) error
 
 	// TODO: これは復習物リポジトリの責任にする
 	// CountGroupedByCategoryByUserID(userID string) ([]*BoxCountGroupedByCategory, error)
--- domain/category/category_repository.go
+++ domain/category/category_repository.go
@@ -1,9 +1,11 @@
 package category
 
-type CategoryRepository interface {
-	Create(category *Category) error
-	GetAllByUserID(userID string) ([]*Category, error)
-	GetByID(categoryID string, userID string) (*Category, error)
-	Update(category *Category) error
-	Delete(categoryID string, userID string) error
+import "context"
+
+type ICategoryRepository interface {
+	Create(ctx context.Context, category *Category) error
+	GetAllByUserID(ctx context.Context, userID string) ([]*Category, error)
+	GetByID(ctx context.Context, categoryID string, userID string) (*Category, error)
+	Update(ctx context.Context, category *Category) error
+	Delete(ctx context.Context, categoryID string, userID string) error
 }
