d39da382cbeac0b02f87606671f67ac2281bc4c8
Masahiro Yamamoto <cmvng00@gmail.com>
2025-06-08 05:12:49 +0900

    feat: 復習物関連の機能のコントローラー層を実装

---
+++ controller/item/item_interface.go
@@ -0,0 +1,26 @@
+package item
+
+import "github.com/labstack/echo/v4"
+
+type IItemController interface {
+	CreateItem(c echo.Context) error
+	UpdateItem(c echo.Context) error
+	UpdateReviewDates(c echo.Context) error
+	UpdateItemAsFinishedForce(c echo.Context) error
+	UpdateReviewDateAsCompleted(c echo.Context) error
+	UpdateReviewDateAsInCompleted(c echo.Context) error
+	UpdateItemAsUnFinishedForce(c echo.Context) error
+	DeleteItem(c echo.Context) error
+
+	GetAllUnFinishedItemsByBoxID(c echo.Context) error
+	GetAllUnFinishedUnclassifiedItemsByUserID(c echo.Context) error
+	GetAllUnFinishedUnclassifiedItemsByCategoryID(c echo.Context) error
+
+	CountItemsGroupedByBoxByUserID(c echo.Context) error
+	CountUnclassifiedItemsGroupedByCategoryByUserID(c echo.Context) error
+	CountUnclassifiedItemsByUserID(c echo.Context) error
+
+	CountDailyDatesGroupedByBoxByUserID(c echo.Context) error
+	CountDailyDatesUnclassifiedGroupedByCategoryByUserID(c echo.Context) error
+	CountDailyDatesUnclassifiedByUserID(c echo.Context) error
+}
