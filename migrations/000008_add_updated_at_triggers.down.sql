d7be72ece70432309eee1d14fdba749a1168ddad
Masahiro Yamamoto <cmvng00@gmail.com>
2025-06-08 05:38:13 +0900

    fix: テーブル名変更による影響を反映

--- migrations/000008_add_updated_at_triggers.down.sql
+++ migrations/000008_add_updated_at_triggers.down.sql
@@ -6,7 +6,7 @@ BEGIN
     DROP TRIGGER IF EXISTS trigger_set_updated_at ON review_boxes;
     DROP TRIGGER IF EXISTS trigger_set_updated_at ON review_items;
     DROP TRIGGER IF EXISTS trigger_set_updated_at ON pattern_steps;
-    DROP TRIGGER IF EXISTS trigger_set_updated_at ON scheduled_review_dates;
+    DROP TRIGGER IF EXISTS trigger_set_updated_at ON review_dates;
 END;
 $$;
 
