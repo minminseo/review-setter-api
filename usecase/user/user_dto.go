58a051969b5b381e878a0c36ad1395481bccc303
Masahiro Yamamoto <cmvng00@gmail.com>
2025-06-08 05:47:48 +0900

    fix: dtoの移動

---
+++ usecase/user/user_dto.go
@@ -0,0 +1,47 @@
+package user
+
+type CreateUserInput struct {
+	Email      string
+	Password   string
+	Timezone   string
+	ThemeColor string
+	Language   string
+}
+
+type CreateUserOutput struct {
+	ID    string
+	Email string
+}
+
+type LoginUserInput struct {
+	Email    string
+	Password string
+}
+
+type LoginUserOutput struct {
+	Token      string
+	ThemeColor string
+	Language   string
+}
+
+type GetUserOutput struct {
+	Email      string
+	Timezone   string
+	ThemeColor string
+	Language   string
+}
+
+type UpdateUserInput struct {
+	ID         string
+	Email      string
+	Timezone   string
+	ThemeColor string
+	Language   string
+}
+
+type UpdateUserOutput struct {
+	Email      string
+	Timezone   string
+	ThemeColor string
+	Language   string
+}
--- usecase/user/user_usercase.go
+++ usecase/user/user_usercase.go
@@ -10,52 +10,6 @@ import (
 	userDomain "github.com/minminseo/recall-setter/domain/user"
 )
 
-type CreateUserInput struct {
-	Email      string
-	Password   string
-	Timezone   string
-	ThemeColor string
-	Language   string
-}
-
-type CreateUserOutput struct {
-	ID    string
-	Email string
-}
-
-type LoginUserInput struct {
-	Email    string
-	Password string
-}
-
-type LoginUserOutput struct {
-	Token      string
-	ThemeColor string
-	Language   string
-}
-
-type GetUserOutput struct {
-	Email      string
-	Timezone   string
-	ThemeColor string
-	Language   string
-}
-
-type UpdateUserInput struct {
-	ID         string
-	Email      string
-	Timezone   string
-	ThemeColor string
-	Language   string
-}
-
-type UpdateUserOutput struct {
-	Email      string
-	Timezone   string
-	ThemeColor string
-	Language   string
-}
-
 type userUsecase struct {
 	userRepo userDomain.UserRepository
 	// transactionManager transaction.ITransactionManager
