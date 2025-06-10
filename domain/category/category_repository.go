04a9cfa44a38494a7249647b876de4f144a57638
Masahiro Yamamoto <cmvng00@gmail.com>
2025-06-10 23:56:50 +0900

    feat daily取得用にGetCategoryNamesByCategoryIDs追加

--- domain/category/category_repository.go
+++ domain/category/category_repository.go
@@ -2,10 +2,18 @@ package category
 
 import "context"
 
+type CategoryName struct {
+	ID   string
+	Name string
+}
+
 type ICategoryRepository interface {
 	Create(ctx context.Context, category *Category) error
 	GetAllByUserID(ctx context.Context, userID string) ([]*Category, error)
 	GetByID(ctx context.Context, categoryID string, userID string) (*Category, error)
 	Update(ctx context.Context, category *Category) error
 	Delete(ctx context.Context, categoryID string, userID string) error
+
+	// item_usecaseで使う。カテゴリーの名前を一覧取得する
+	GetCategoryNamesByCategoryIDs(ctx context.Context, categoryIDs []string) ([]*CategoryName, error)
 }
