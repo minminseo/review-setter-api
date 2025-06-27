3e5ec695c3d7ebb74a116a9b2b9f16f8b6720400
Masahiro Yamamoto <cmvng00@gmail.com>
2025-06-28 02:42:57 +0900

    fix: ErrPatternRelatedToItemを削除と更新の2つに文章

--- domain/pattern/error.go
+++ domain/pattern/error.go
@@ -3,7 +3,8 @@ package pattern
 import "errors"
 
 var (
-	ErrNoDiff               = errors.New("変更点がありません")
-	ErrPatternNotFound      = errors.New("復習パターンが存在しません")
-	ErrPatternRelatedToItem = errors.New("この復習パターンは復習物に紐づいているため削除できません")
+	ErrNoDiff                     = errors.New("変更点がありません")
+	ErrPatternNotFound            = errors.New("復習パターンが存在しません")
+	ErrPatternRelatedToItemDelete = errors.New("この復習パターンは復習物に紐づいているため削除できません")
+	ErrPatternRelatedToItemUpdate = errors.New("この復習パターンは復習物に紐づいているため変更できません")
 )
